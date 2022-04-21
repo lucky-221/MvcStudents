8d821d0454549e7f5958d6774f9fdf95bb2f8836
Admin <Admin@LAPTOP-E38PGBBF>
2022-04-21 14:39:39 +0800

    添加项目文件。

---
+++ MvcStudents/Data/MvcStudentsContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcStudents.Models;
+
+namespace MvcStudents.Data
+{
+    public class MvcStudentsContext : DbContext
+    {
+        public MvcStudentsContext (DbContextOptions<MvcStudentsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcStudents.Models.Student> Student { get; set; }
+    }
+}
---
+++ MvcStudents/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcStudents.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [StringLength(300)]
+        public string? Name { get; set; }
+
+        [StringLength(1)]
+        public string? Gender { get; set; }
+
+        [Range(0, 200, ErrorMessage = "年龄在0-200之间")]
+        public int Age { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime BirthdayDate { get; set; }
+        [StringLength(13)]
+        public string? Phone { get; set; }
+
+        [Range(30, 50)]
+        public decimal Temperature { get; set; }
+        
+        public DateTime TodayDate { get; set; }
+    }
+}
